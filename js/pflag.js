55ec7141002bc50fda474aa99b18fba7ee8659ab
David Manuel <dmanuel@pflag.org>
2014-11-20 04:13:52 -0800

    initial commit

---
+++ js/pflag.js
@@ -0,0 +1,9 @@
+$().ready(function() {
+
+    //bottom align HR rules by logo
+    $("#logo-header hr").css("margin-top",$("#logo").height()*0.775);
+
+    //spread out nav buttons
+/*    var nav_li_width = $("#bs-example-navbar-collapse-1").width()/$("#bs-example-navbar-collapse-1 li").length
+    $("#bs-example-navbar-collapse-1 li").each(function(k,v) {$(v).width(nav_li_width)});*/
+});
